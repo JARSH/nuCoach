6b48d4431ae0ad563b2bb3371131349e5d1d6186
Shrey-Sachdeva <shreysachdeva@utexas.edu>
2020-02-03 13:53:43 -0600

    Added some classes, basic foot pressure functionality

---
+++ lib/rep.dart
@@ -0,0 +1,9 @@
+import 'package:nucoach/angles.dart';
+import 'package:nucoach/foot_pressure_map.dart';
+
+class Rep {
+  int repid;
+  int score;
+  Angles angles;
+  FootPressureMap fpmap;
+}
---
+++ lib/set.dart
@@ -0,0 +1,9 @@
+import 'package:nucoach/exercise.dart';
+import 'package:nucoach/rep.dart';
+
+class Set {
+  int setid;
+  Exercise exercise;
+  int score;
+  List<Rep> reps;
+}
