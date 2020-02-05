6e21c249e344af52f0afec8db493bf61ca51c78f
haisunbanh <haisun.banh@utexas.edu>
2020-02-05 12:28:24 -0600

    dependencies on camera, tflite plugins, added camera and session start widgets

---
+++ lib/session_start.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'camera_widget.dart';
+import 'package:camera/camera.dart';
+
+class Session extends StatefulWidget {
+  final Color color;
+  final String text;
+  final TextStyle style;
+  final List<CameraDescription> cameras;
+
+  Session(this.color, this.text, this.style, this.cameras);
+
+  @override
+  SessionState createState() => new SessionState();
+}
+
+class SessionState extends State<Session> {
+  List<dynamic> _recognitions;
+  int _imageHeight = 0;
+  int _imageWidth = 0;
+
+  setRecognitions(recognitions, imageHeight, imageWidth) {
+    setState(() {
+      _recognitions = recognitions;
+      _imageHeight = imageHeight;
+      _imageWidth = imageWidth;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        // print('you clicked me');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Camera(widget.cameras, setRecognitions)),
+        );
+      },
+      color: widget.color,
+      child: Text(
+        widget.text,
+        style: widget.style,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
