072aea46dd510fc5264ffabcbdf24de537c3126a
haisunbanh <haisun.banh@utexas.edu>
2020-03-30 12:55:09 -0500

    broken state but with camera controller turned off while showing dialogs

--- lib/screens/camera/camera_widget.dart
+++ lib/screens/camera/camera_widget.dart
@@ -8,6 +8,9 @@ import 'package:nucoach/models/session.dart';
 import 'package:nucoach/enums/exercise.dart';
 import 'package:nucoach/screens/summary/summary_widget.dart';
 import 'package:tflite/tflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'dart:io';
 
 enum ConfirmSave { DISCARD, KEEP, EXPORT }
 enum MidSession { END, CONTINUE }
@@ -32,6 +35,10 @@ class _CameraState extends State<Camera> {
   bool isDetecting = false;
   Future<Session> session;
   bool calculating = false;
+  bool waiting = true;    //waiting for user to click "DONE"
+  String videoPath;
+  Directory dir;
+  bool _cameraOn = true;  //allows the controller to be 'off' while midsession dialogs are displayed
 
   //variables for rep detection
   var previousData;
@@ -51,6 +58,7 @@ class _CameraState extends State<Camera> {
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await showDialog<String>(
         context: context,
+        barrierDismissible: false,
         builder: (BuildContext context) => new AlertDialog(
           // title: new Text("title"),
           content: new Text(
@@ -59,6 +67,8 @@ class _CameraState extends State<Camera> {
             new FlatButton(
               child: new Text("DONE"),
               onPressed: () {
+                waiting = false;
+                //startVideoRecording();
                 Navigator.of(context).pop();
               },
             ),
@@ -71,7 +81,7 @@ class _CameraState extends State<Camera> {
       print('No camera is found');
     } else {
       controller = new CameraController(
-        widget.cameras[0], //front facing camera = 1, rear camera = 0
+        widget.cameras[1], //front facing camera = 1, rear camera = 0
         ResolutionPreset.medium,
       );
       controller.initialize().then((_) {
@@ -81,8 +91,12 @@ class _CameraState extends State<Camera> {
         setState(() {});
 
         controller.startImageStream((CameraImage img) {
-          if (!isDetecting && !calculating) {
+          if (!isDetecting && !calculating && !waiting) {
             isDetecting = true;
+
+            if(firstFrame) {
+              startVideoRecording();
+            }
 
             var result = Tflite.runPoseNetOnFrame(
               bytesList: img.planes.map((plane) {
@@ -94,6 +108,7 @@ class _CameraState extends State<Camera> {
             ).then((recognitions) {
               var keyData = recognitions[0]['keypoints'];
               if (firstFrame) {
+
                 //check scores of leftHip and rightHip, and decide which will be the reference
                 if (keyData[11]['score'] > keyData[12]['score']) {
                   //TODO: deal with exceptions
@@ -109,7 +124,7 @@ class _CameraState extends State<Camera> {
                   descending = false;
                   //send previousData to a local buffer to be processed later
                   angleBuffer.add(previousData);
-                  widget.currentReps++;
+                  widget.currentReps++;   //moved to calculateAngles
                 } else if (!descending &&
                     (keyData[referenceID]['y'] >
                         previousData[referenceID]['y'])) {
@@ -125,6 +140,65 @@ class _CameraState extends State<Camera> {
         });
       });
     }
+  }
+
+ Future<String> startVideoRecording() async {
+    if (!controller.value.isInitialized) {
+      return null;
+    }
+
+    	
+    // Do nothing if a recording is on progress
+    if (controller.value.isRecordingVideo) {
+      return null;
+    }
+
+    //final Directory extDir = await getApplicationDocumentsDirectory();
+    //final String dirPath = '${extDir.path}/Movies/flutter_test';
+
+    final Directory appDirectory = await getExternalStorageDirectory();
+    final String videoDirectory = '${appDirectory.path}/Videos';
+    await Directory(videoDirectory).create(recursive: true);
+    final String currentTime = DateTime.now().millisecondsSinceEpoch.toString();
+    final String filePath = '$videoDirectory/$currentTime.mp4';
+
+    try {
+      videoPath = filePath;
+      dir = new Directory(videoDirectory);
+      await controller.startVideoRecording(filePath);
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+    return filePath;
+  }
+
+  Future<void> stopVideoRecording() async {
+    if (!controller.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      await controller.stopVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+
+  }
+
+  // void onStartRecording() {
+  //   startVideoRecording().then((String saveLocation) {
+  //     if (mounted) setState(() {});
+  //     if (saveLocation != null) showInSnackBar('Saving video to $saveLocation');
+  //   });
+  // }
+
+  	
+  void _showCameraException(CameraException e) {
+    String errorText = 'Error: ${e.code}\nError Message: ${e.description}';
+    print(errorText);
+
   }
 
   @override
@@ -169,7 +243,6 @@ class _CameraState extends State<Camera> {
       knee = 14;
       ankle = 16;
     }
-    //TODO: check this loop
     if (widget.currentReps != 0) {
       Session s = await session;
       Map<String, dynamic> row = {
@@ -180,7 +253,6 @@ class _CameraState extends State<Camera> {
       };
       final id = await dbHelper.insertSet(row);
       for (var angleData in angleBuffer) {
-        //TODO: concurrent modification exception (growable list)
         double shoulderHip = math.sqrt(
             math.pow((angleData[shoulder]['x'] - angleData[hip]['x']), 2) +
                 math.pow((angleData[shoulder]['y'] - angleData[hip]['y']), 2));
@@ -207,10 +279,14 @@ class _CameraState extends State<Camera> {
         Map<String, dynamic> row = {
           columnSetId: id,
           columnScore: 67, //DUMMY VALUE
-          columnShk: (shk*180/math.pi).round(),
-          columnHka: (hka*180/math.pi).round(),
+          columnShk: shk,
+          columnHka: hka,
         };
-        await dbHelper.insertRep(row);
+        //if (.17453 <= shk && shk <= 2.0944 && .0349066 <= hka && hka <= 2.0944) {//angles dont suck
+          //in degrees, 10<shk<120 and 20<hka<120
+          await dbHelper.insertRep(row);
+          //widget.currentReps++;
+        //}
       }
       angleBuffer = [];
     }
@@ -269,6 +345,12 @@ class _CameraState extends State<Camera> {
               child: const Text('CONTINUE WORKING OUT'),
               onPressed: () {
                 Navigator.of(context).pop(MidSession.CONTINUE);
+                setState(() {
+                  _cameraOn = true;
+                });
+                startVideoRecording();
+
+                //TODO: should bring up alert dialog again here to reposition
               },
             ),
           ],
@@ -303,33 +385,38 @@ class _CameraState extends State<Camera> {
         maxWidth: screenRatio > previewRatio
             ? screenH / previewH * previewW
             : screenW,
-        child: CameraPreview(controller),
+        child: _cameraOn ? CameraPreview(controller) : Container(),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.redAccent,
         onPressed: () async {
+          //TODO: should turn off camera controller while asking for dialogs
+          stopVideoRecording();
+          setState(() {
+            _cameraOn = false;
+          });
           // open dialog to discard, keep, or keep and export data
           calculating = true;
           final ConfirmSave action = await _asyncConfirmDialog(context);
           if (action == ConfirmSave.KEEP || action == ConfirmSave.EXPORT) {
-            // TODO: process local buffer of angles and send to database
-            widget.totalReps += widget.currentReps;
-            //store to database
             CalculateAngles();
+            widget.totalReps += widget.currentReps;
             if (action == ConfirmSave.EXPORT) {
-              // TODO: export video
-            }
-            final MidSession next = await _asyncMidSessDialog(context);
-            if (next == MidSession.END) {
-              Session se = await session;
-              Get.to(SummaryWidget(se));
-            } else {
-              widget.currentReps = 0;
-              //firstFrame = true;
-              isDetecting = false;
-              calculating = false;
+              // TODO: check this line
+              GallerySaver.saveVideo(videoPath);
             }
           }
+          final MidSession next = await _asyncMidSessDialog(context);
+          if (next == MidSession.END) {
+            Session se = await session;
+            Get.to(SummaryWidget(se));
+          } else {
+            widget.currentReps = 0;
+            isDetecting = false;
+            calculating = false;
+          }
+          //TODO: clear saved video from app storage somehow? (check this)
+          dir.deleteSync(recursive: true);
         },
         child: Icon(Icons.stop),
       ),
